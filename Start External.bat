e89b333ad6b871f7f5c1c52ea6da5066d2f47a69
KaspaNinja <153366933+LiveLaughLove13@users.noreply.github.com>
2026-01-04 02:02:55 -0500

    remove bat

--- Start External.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-echo Starting stratum-bridge in EXTERNAL mode...
-stratum-bridge.exe --node-mode external
-pause
--- start-bridge-external.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-echo Starting stratum-bridge in external mode...
-stratum-bridge.exe --config config.yaml --node-mode external
-pause
